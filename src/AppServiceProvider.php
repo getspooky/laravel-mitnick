45fc770f696c0cacfb511c5be0df86f48fe49456
yasser <ameur@netcourrier.com>
2019-07-05 12:50:28 +0200

    first commit

---
+++ src/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Mitnick\Laravel;
+
+class AppServiceProvider
+{
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+     public function register()
+     {
+        //
+     }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+     public function boot()
+     {
+        //
+
+     }
+
+
+}
